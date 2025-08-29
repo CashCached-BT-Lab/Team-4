a0c39ce7b2af83741caf8592b4a68d98f9245d41
Megh Patel <meghp@Meghs-MacBook-Air.local>
2025-08-29 09:39:32 +0530

    Base App

---
+++ src/main/java/edu/manipal/btlab/BtlabApplication.java
@@ -0,0 +1,13 @@
+package edu.manipal.btlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/manipal/btlab/BtlabApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.manipal.btlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
